741e3ba4f535e95e8196cabd3978dabac02727a2
christinechoi <smi77y@gmail.com>
2016-12-17 14:41:34 -0500

    Add forms to user login and signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,36 @@ class UsersController < ApplicationController
 
 
 
+  get '/users/signup' do 
+    erb :'/users/signup'
+  end
+
+
+  post '/users/signup' do 
+
+    @user = User.create(params)
+    session[:id] = user.id
+
+    redirect :'/collections'
+  end
+
+
+  get '/users/login' do 
+    erb :'/users/login'
+  end
+
+
+  post '/users/login' do 
+
+
+  end
+
+  get '/users/:slug' do 
+
+    erb :'/users/show'
+  end
+
+
+
 
 end
