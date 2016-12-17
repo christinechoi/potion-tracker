a29bce6b56b3219bfe2b7367370a9d7db418cf4d
christinechoi <smi77y@gmail.com>
2016-12-16 22:17:41 -0500

    Add routes to collections controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+require './config/environment'
+require 'pry'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  enable :sessions
+
+
+
+
+
+
+
+
+
+end
---
+++ app/controllers/collections_controller.rb
@@ -0,0 +1,36 @@
+class CollectionsController < ApplicationController 
+
+  get '/collections' do 
+    @collections = Collection.all
+
+    erb :index
+  end
+
+
+  get '/collections/new' do 
+
+
+
+    erb :'collections/new'
+  end
+
+
+  get '/collections/:id/edit' do 
+
+  end
+
+
+  get '/collections/:id' do 
+
+  end
+
+
+  post '/collections/:id' do 
+
+  end
+
+
+
+
+
+end
