e464136d15201ec7d43d2a652f94603697a64cef
christinechoi <smi77y@gmail.com>
2017-05-23 22:06:13 -0400

    Add validations to Collection and Product models

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,6 +1,7 @@
-
 class Collection < ActiveRecord::Base
   belongs_to :user
   has_many :collection_products
   has_many :products, through: :collection_products
+
+  validates :name, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
-
 class Product < ActiveRecord::Base
   #has_many :users, through: :collections
   has_many :collections, through: :collection_products
   has_many :collection_products
+
+  validates :name, presence: true
+  validates :brand, uniqueness: true
+  validates :description, presence: true
+
 end
