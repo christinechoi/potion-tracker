9a306089228152d03db5b3f7b76ad2594695e975
christinechoi <smi77y@gmail.com>
2017-05-23 22:17:15 -0400

    Add if/else statements to handle nonexistent collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -7,39 +7,46 @@ class CollectionsController < ApplicationController
     erb :'collections/index'
   end
 
-
   get '/collections/new' do 
     redirect_if_not_logged_in
 
     erb :'collections/new'
   end
 
-
   get '/collections/:id/edit' do 
     redirect_if_not_logged_in
-    @collection = Collection.find(params[:id])
-
-     erb :'/collections/edit'
+    @collection = Collection.find_by(id: params[:id])
+    if @collection && @collection.user == current_user
+      erb :'/collections/edit'
+    else 
+      flash[:notice] = "You are a horrible person"
+      redirect_to "/collections"
+    end
   end
 
 
   get '/collections/:id' do 
     redirect_if_not_logged_in
     
-    @collection = Collection.find(params[:id])
-    @products = @collection.products
+    @collection = Collection.find_by(id: params[:id])
+    @products = @collection.products if @collection 
 
     erb :'/collections/show'
   end
 
 
   post '/collections' do 
-    # binding.pry
-    @collection = Collection.create(name: params[:name], user_id: params[:user_id])
-    flash.now[:notice] = "Successfully created new collection."
-    erb :'/collections/show'
+    redirect_if_not_logged_in
+    @collection = current_user.collections.create(name: params[:name])
+    
+    if @collection.save 
+      flash[:notice] = "Successfully added new collection."
+      redirect "/collections/show"
+    else
+      flash[:notice] = "Collection must have a name."
+      erb :"/collections/new" 
+    end
+
   end
-
-
 
 end
